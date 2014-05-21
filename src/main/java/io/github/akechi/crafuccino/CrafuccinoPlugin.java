106d4fcfbe71c28edc5adb053cfc6922bf95c51c
ujihisa <ujihisa@gmail.com>
2014-05-20 21:31:41 -0700

    js worked!

--- src/main/java/io/github/akechi/crafuccino/CrafuccinoPlugin.java
+++ src/main/java/io/github/akechi/crafuccino/CrafuccinoPlugin.java
@@ -1,9 +1,21 @@
 package io.github.akechi.crafuccino.CrafuccinoPlugin;
 
 import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
 
 public class CrafuccinoPlugin {
+  Context cx;
+
   public static void main(String[] args) {
-    System.out.print("hi");
+    new CrafuccinoPlugin().f();
+  }
+
+  // just for now
+  public void f() {
+    this.cx = Context.enter();
+    Scriptable scope = this.cx.initStandardObjects();
+    String s = "(function(x, y) { return x + 2 * y; })(10, 20)";
+    Object result = cx.evaluateString(scope, s, "<cmd>", 1, null);
+    System.out.print(result);
   }
 }
