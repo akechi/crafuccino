9f6617fdf36ce6854637e4537e64396da2354ee9
ujihisa <ujihisa@gmail.com>
2014-05-20 21:26:42 -0700

    init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "crafuccino"
+
+version := "1.0"
+
+libraryDependencies += "rhino" % "js" % "1.7R2"
---
+++ src/main/java/io/github/akechi/crafuccino/CrafuccinoPlugin.java
@@ -0,0 +1,9 @@
+package io.github.akechi.cjaft.CjaftPlugin;
+
+import org.mozilla.javascript.Context;
+
+public class CjaftPlugin {
+  public static void main(String[] args) {
+    System.out.print("hi");
+  }
+}
